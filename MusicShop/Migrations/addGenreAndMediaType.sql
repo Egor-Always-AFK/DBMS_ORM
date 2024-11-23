501774103273fda5af4b47490f287dc6fd6185d1
Egor Chayka <52970978+Egor-Always-AFK@users.noreply.github.com>
2024-11-23 15:07:59 +0300

    add sql-migration script

---
+++ MusicShop/Migrations/addGenreAndMediaType.sql
@@ -0,0 +1,22 @@
+﻿insert into genres (genre_id, name) values 
+                                        (1, 'Rock'),
+                                        (2, 'Pop'),
+                                        (3, 'R&B'),
+                                        (4, 'Folk'),
+                                        (5, 'Rap'),
+                                        (6, 'Indie'),
+                                        (7, 'Alternative')
+on conflict do nothing;
+
+insert into media (media_id, type) values 
+                                       (1, 'Disk'),
+                                       (2, 'Vinyl'),
+                                       (3, 'CD'),
+                                       (4, 'Cloud')
+on conflict do nothing;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20241123094557_addGenreAndMediaType', '9.0.1');
+
+COMMIT;
+
