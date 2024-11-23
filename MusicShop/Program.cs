c752170519a7274f77aaa2725530f7c9699ac001
Egor Chayka <52970978+Egor-Always-AFK@users.noreply.github.com>
2024-11-23 14:40:58 +0300

    Add sql-script migrations

--- MusicShop/Program.cs
+++ MusicShop/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System.Formats.Asn1;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MusicShop.config;
 using MusicShop.repository;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +21,37 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    Console.WriteLine("Applying EF Core migrations...");
     dbContext.Database.Migrate();
+    Console.WriteLine("EF Core migrations have been successfully applied.");
+    
+    var sqlMigrationsFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Migrations");
+    if (Directory.Exists(sqlMigrationsFolder))
+    {
+        Console.WriteLine("Applying SQL script migrations...");
+        var connectionString = dbContext.Database.GetConnectionString();
+
+        foreach (var sqlFile in Directory.GetFiles(sqlMigrationsFolder, "*.sql"))
+        {
+            Console.WriteLine($"Executing sql file: {sqlFile}");
+            var sql = File.ReadAllText(sqlFile);
+
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            Console.WriteLine($"SQL script {Path.GetFileName(sqlFile)} migrations have been successfully applied.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("No Sql-script folder found.");
+    }
 }
 
 app.MapControllers();
